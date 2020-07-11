659607303c8e5306c30763a4deca701534becbff
Tom A <tomlaurenceallison@gmail.com>
2020-07-11 14:13:03 +0100

    mongoose

---
+++ main/js/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/todoDB", {useNewUrlParser: true});
+module.exports = {
+    module = []
+
+    module.exports = {
+        mongoose;
+    }
+}
