a48b4ca51a196edbf966e51ac46923fbf13760c1
Tom A <tomlaurenceallison@gmail.com>
2020-07-11 12:12:27 +0100

    Added req.params to list route

--- main/routes/routes.js
+++ main/routes/routes.js
@@ -3,30 +3,39 @@ const router = express.Router()
 const date = require('../js/date');
 const db = require('../../app').db
 
-let tasks = ['Buy food', 'Cook food', 'Eat food'];
-let worktasks = ['Study']
+
+let lists = {
+    'tasks': ['Buy food', 'Cook food', 'Eat food'],
+    'work': ['Study']
+}
 
 
 router.route('/')
     .get((req, res) => {
         today = date.getToday();
-        return res.render('../views/list', { title: "ToDo List", today: today, tasks: tasks, formAction: "/" });
+        return res.render('../views/list', { title: "ToDo List", today: today, list: lists['tasks'], formAction: "/" });
     })
     .post((req, res) => {
         if (req.body.taskBody !== "") {
-            tasks.push(req.body.taskBody);
+            lists.tasks.push(req.body.taskBody);
             return res.redirect('/');
         }
     });
 
-router.route('/work')
+router.route('/:list')
     .get((req, res) => {
+        let list = req.params.list;
         today = date.getToday();
-        return res.render('../views/list', { title: "ToDo List", today: today, tasks: worktasks, formAction: "/work" });
+        if(list in lists){
+            return res.render('../views/list', { title: "ToDo List", today: today, list: lists[list], formAction: "/work" });
+        } else {
+            return res.redirect('/');
+        }
     })
     .post((req, res) => {
         if (req.body.taskBody !== "") {
-            worktasks.push(req.body.taskBody);
+            let list = req.params.list;
+            lists.list.push(req.body.taskBody);
             return res.redirect('/work');
         }
     });
